14b9da5f2fffb285bc89a7b41e38df54c4a06af9
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-29 12:10:24 -0300

    Criado módulo core e transferido componente NavbarComponent.
    Este módulo tem a finalidade conter todos os módulos e componentes utilizados em toda a aplicação.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
+import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
-import { NavbarComponent } from './navbar/navbar.component';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
@@ -14,14 +13,14 @@ import { PessoasModule } from './pessoas/pessoas.module';
 @NgModule({
   declarations: [
     AppComponent,
-
-    NavbarComponent
   ],
   exports: [
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
+    CoreModule,
 
     LancamentosModule,
     PessoasModule,
