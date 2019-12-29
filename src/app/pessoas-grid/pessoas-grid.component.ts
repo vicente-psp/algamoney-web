c2d723d3f00c28eb7c0f36a8839ea83fdc26015e
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-29 00:51:17 -0300

    Implementando desafio AW (transferido table do componente pessoas-pesquisa para o componente pessoas-grid).

--- src/app/pessoas-grid/pessoas-grid.component.ts
+++ src/app/pessoas-grid/pessoas-grid.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pessoas-grid',
   templateUrl: './pessoas-grid.component.html',
   styleUrls: ['./pessoas-grid.component.css']
 })
-export class PessoasGridComponent implements OnInit {
+export class PessoasGridComponent {
 
-  constructor() { }
+  @Input() pessoas = [];
 
-  ngOnInit() {
+
+  public getAtivoStr(ativo: boolean): string {
+    return ativo ? 'Ativo' : 'Inativo';
   }
 
 }
