afc6f21230958d1b86c10e4096812a47bd502fb7
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-31 01:48:54 -0300

    Removido dados de lancamentos estáticos para receber dados da api.

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,4 +1,4 @@
-import { LancamentosPesquisaService } from './lancamentos-pesquisa.service';
+import { LancamentosService } from './../lancamentos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,26 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent implements OnInit {
 
-  lancamentos = [
-    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 7, 30),
-      dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
-    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2017, 7, 10),
-      dataPagamento: new Date(2017, 7, 9), valor: 80000, pessoa: 'Atacado Brasil' },
-    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(2017, 8, 20),
-      dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda' },
-    { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: new Date(2017, 7, 5),
-      dataPagamento: new Date(2017, 6, 30), valor: 800, pessoa: 'Escola Abelha Rainha' },
-    { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: new Date(2017, 9, 18),
-      dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza' },
-    { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: new Date(2017, 8, 10),
-      dataPagamento: new Date(2017, 8, 9), valor: 1750, pessoa: 'Casa Nova Imóveis' },
-    { tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2017, 8, 13),
-      dataPagamento: null, valor: 180, pessoa: 'Academia Top' }
-  ];
+  lancamentos = [];
 
-  constructor() { }
+  constructor(private lancamentosService: LancamentosService) { }
 
   ngOnInit() {
+    this.listar();
+  }
+
+  private listar(): void {
+    this.lancamentosService.getLista().subscribe(
+      data => {
+        console.log(data[1]);
+        this.lancamentos = data;
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
 
 }
