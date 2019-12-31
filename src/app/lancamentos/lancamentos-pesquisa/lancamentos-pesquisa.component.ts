84c9ba1f991f046a1c9345559611ec3b9791066e
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-31 18:57:07 -0300

    Implementado a pesquisa no keyup da descrição.
    Para buscar os dados dinamicamente.

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { LazyLoadEvent } from 'primeng/api/public_api';
 
 import { LancamentosService, LancamentoFiltro } from './../lancamentos.service';
+import { Subscription, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 
 @Component({
@@ -10,22 +12,34 @@ import { LancamentosService, LancamentoFiltro } from './../lancamentos.service';
   templateUrl: './lancamentos-pesquisa.component.html',
   styleUrls: ['./lancamentos-pesquisa.component.css']
 })
-export class LancamentosPesquisaComponent implements OnInit {
+export class LancamentosPesquisaComponent implements OnInit, OnDestroy {
 
   public totalRegistros = 0;
   public lancamentos = [];
   public filtro = new LancamentoFiltro();
 
+  private subscriptionDescricao: Subscription;
+  private subjectDescricao: Subject<string> = new Subject();
+
   constructor(private lancamentosService: LancamentosService) { }
 
   ngOnInit() {
+    this.subscriptionDescricao = this.subjectDescricao.pipe(
+      debounceTime(400),
+      distinctUntilChanged()
+    ).subscribe(
+      () => this.listar()
+    );
   }
 
-  private listar(pagina = 0): void {
+  ngOnDestroy() {
+    this.subscriptionDescricao.unsubscribe();
+  }
+
+  public listar(pagina = 0): void {
     this.filtro.pagina = pagina;
     this.lancamentosService.getLista(this.filtro).subscribe(
       data => {
-        console.log('data: ', data);
         this.lancamentos = data.content;
         this.totalRegistros = data.total;
       },
@@ -38,6 +52,10 @@ export class LancamentosPesquisaComponent implements OnInit {
   public nextPage(event: LazyLoadEvent): void {
     const pagina = event.first / event.rows;
     this.listar(pagina);
+  }
+
+  public keyupDescricao(): void {
+    this.subjectDescricao.next(this.filtro.descricao);
   }
 
 }
