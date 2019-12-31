8bc57b35b783439317466e3a34d4010b5f1ba823
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-31 18:59:37 -0300

    Implementado paginação em pesquisar pessoas + busca pelo keyup do nome.
    Para buscar os dados dinamicamente.

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,28 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { LazyLoadEvent } from 'primeng/api/public_api';
+
+import { PessoaFiltro, PessoasService } from './../pessoas.service';
+import { Subscription, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-pessoas-pesquisa',
   templateUrl: './pessoas-pesquisa.component.html',
   styleUrls: ['./pessoas-pesquisa.component.css']
 })
-export class PessoasPesquisaComponent implements OnInit {
+export class PessoasPesquisaComponent implements OnInit, OnDestroy {
 
-  pessoas = [
-    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG', ativo: true },
-    { nome: 'Sebastião da Silva', cidade: 'São Paulo', estado: 'SP', ativo: false },
-    { nome: 'Carla Souza', cidade: 'Florianópolis', estado: 'SC', ativo: true },
-    { nome: 'Luís Pereira', cidade: 'Curitiba', estado: 'PR', ativo: true },
-    { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro', estado: 'RJ', ativo: false },
-    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true }
-  ];
+  public totalRegistros = 0;
+  public pessoas = [];
+  public filtro = new PessoaFiltro();
 
-  constructor() { }
+  private subscriptionNome: Subscription;
+  private subjectNome: Subject<string> = new Subject();
+
+  constructor(private pessoasService: PessoasService) { }
 
   ngOnInit() {
+    this.subscriptionNome = this.subjectNome.pipe(
+          debounceTime(400),
+          distinctUntilChanged()
+        ).subscribe(
+          () => this.listar()
+        );
+  }
+
+  ngOnDestroy() {
+    this.subscriptionNome.unsubscribe();
+  }
+
+  public listar(pagina = 0): void {
+    this.filtro.pagina = pagina;
+    this.pessoasService.getLista(this.filtro).subscribe(
+      data => {
+        this.pessoas = data.content;
+        this.totalRegistros = data.total;
+      }
+    );
+  }
+
+  public nextPage(event: LazyLoadEvent): void {
+    const pagina = event.first / event.rows;
+    this.listar(pagina);
   }
 
   public getAtivoStr(ativo: boolean): string {
     return ativo ? 'Ativo' : 'Inativo';
+  }
+
+  public keyupNome(): void {
+    this.subjectNome.next(this.filtro.nome);
   }
 
 }
