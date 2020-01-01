7f6cc03a990ac83c18f36352978a53968f9c4415
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-01 15:03:40 -0300

    Criado serviço para tratar erros.

---
+++ src/app/core/error-handler.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ToastyService } from 'ng2-toasty';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlerService {
+
+  constructor(
+    private toastyService: ToastyService
+  ) { }
+
+  public handleError(errorResponse: any): void {
+    let msg: string;
+    if (typeof errorResponse === 'string') {
+      msg = errorResponse;
+    } else {
+      msg = 'Erro ao processar serviço remoto. Tente novamente.';
+      console.error('Ocorreu um erro', errorResponse);
+    }
+    this.toastyService.error(msg);
+  }
+}
