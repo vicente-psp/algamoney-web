8a8143a0f2b87dfe146a74294d9959daa7c8c08d
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-01 14:28:05 -0300

    Aidicionado módulos e serviço.
    Adicionado ConfirmDialogModule, LOCALE_ID, localePt, ConfirmationService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { CoreModule } from './core/core.module';
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ToastyModule } from 'ng2-toasty';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
 
-import { AppComponent } from './app.component';
-
+import { CoreModule } from './core/core.module';
+import { AppComponent } from './app.component';4
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -24,6 +28,8 @@ import { PessoasModule } from './pessoas/pessoas.module';
 
     ToastyModule.forRoot(),
 
+    ConfirmDialogModule,
+
     CoreModule,
 
     LancamentosModule,
@@ -31,7 +37,13 @@ import { PessoasModule } from './pessoas/pessoas.module';
 
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ConfirmationService,
+
+    {
+      provide: LOCALE_ID, useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
