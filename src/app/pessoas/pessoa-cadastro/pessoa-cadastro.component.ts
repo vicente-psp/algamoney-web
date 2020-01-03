6094365d138a74dbec4d1b1c1625e815314bef66
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-02 22:02:30 -0300

    Implementado salvar pessoa.
    Criado método salvar no component, adicionado chamada ao método e feito o bind das propriedades no template html.

--- src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { ToastyService } from 'ng2-toasty';
+
+import { PessoasService } from '../pessoas.service';
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { Pessoa } from '../../core/models/pessoa.model';
+
 
 @Component({
   selector: 'app-pessoa-cadastro',
@@ -7,9 +15,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoaCadastroComponent implements OnInit {
 
-  constructor() { }
+  public pessoa: Pessoa = new Pessoa();
+
+  constructor(
+    private pessoasService: PessoasService,
+    private toastyService: ToastyService,
+    private errorHandlerService: ErrorHandlerService
+  ) { }
 
   ngOnInit() {
+  }
+
+  public salvar(form: FormControl): void {
+    this.pessoa.ativo = true;
+    this.pessoasService.salvar(this.pessoa).subscribe(
+      () => {
+        this.toastyService.success('Pessoa salva com sucesso!');
+        form.reset(new Pessoa());
+        this.pessoa = new Pessoa();
+      },
+      err => this.errorHandlerService.handleError(err)
+    );
   }
 
 }
