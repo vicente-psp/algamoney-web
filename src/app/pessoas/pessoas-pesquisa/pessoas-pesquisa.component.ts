73b8baaf11c8324558130787189041cbc01300f9
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-02 22:02:52 -0300

    Removido console.log.

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -92,7 +92,6 @@ export class PessoasPesquisaComponent implements OnInit, OnDestroy {
   }
 
   public alterarStatus(pessoa: any): void {
-    console.log('pessoa: ', pessoa);
     this.pessoasService.alterarStatus(pessoa.id, !pessoa.ativo).subscribe(
       () => {
         this.tabela.reset();
