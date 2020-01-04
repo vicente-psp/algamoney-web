849f1df7b4fe3a5988ad882d0f5bceffd1f2f3d7
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-04 19:46:54 -0300

    Criado método para buscar lancamento pelo id, alterar lancamento, salvar novo lancamento, para verificar se número é válido | no oninit foi inserido instruções para verificar se rota contém um id e chama método de buscar lancamento.

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 import { ToastyService } from 'ng2-toasty';
+import { take } from 'rxjs/operators';
 
 import { CategoriasService } from '../../categorias/categorias.service';
 import { ErrorHandlerService } from './../../core/error-handler.service';
@@ -24,18 +26,47 @@ export class LancamentoCadastroComponent implements OnInit {
     { label: 'Receita', value: 'RECEITA' },
     { label: 'Despesa', value: 'DESPESA' }
   ];
+  public title = 'Novo Lançamento';
 
   constructor(
     private lancamentosService: LancamentosService,
     private errorHandlerService: ErrorHandlerService,
     private categoriasService: CategoriasService,
     private pessoasService: PessoasService,
-    private toastyService: ToastyService
+    private toastyService: ToastyService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
     ) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.pipe(take(1)).subscribe(
+      (params: ParamMap) => {
+        if (!this.lancamentosService.isNullOrUndefined(params.get('id'))) {
+          const id = Number.parseInt(params.get('id'), 10);
+          if (id > 0) {
+            this.getLancamento(id);
+            this.title = 'Edição de Lançamento';
+          }
+        }
+      }
+    );
+
     this.listAllCategorias();
     this.listAllPessoas();
+  }
+
+  private getLancamento(id: number): void {
+    this.lancamentosService.getDados(id).subscribe(
+      (lancamento: Lancamento) => {
+        this.lancamento = lancamento;
+      },
+      err => {
+        this.errorHandlerService.handleError(err);
+        setTimeout(() => {
+          this.router.navigateByUrl('/lancamentos');
+        }, 1000);
+      }
+    );
   }
 
   private listAllCategorias(): void {
@@ -56,6 +87,14 @@ export class LancamentoCadastroComponent implements OnInit {
     );
   }
 
+  public submitForm(form: FormControl): void {
+    if (this.isValidNumber(this.lancamento.id)) {
+      this.alterar();
+    } else {
+      this.salvar(form);
+    }
+  }
+
   public salvar(form: FormControl): void {
     this.lancamentosService.salvar(this.lancamento).subscribe(
       () => {
@@ -65,6 +104,30 @@ export class LancamentoCadastroComponent implements OnInit {
       },
       err => this.errorHandlerService.handleError(err)
     );
+  }
+
+  public alterar(): void {
+    this.lancamentosService.alterar(this.lancamento, this.lancamento.id).subscribe(
+      () => {
+        this.toastyService.success('Lançamento atualizado com sucesso');
+        setTimeout(() => {
+          this.router.navigateByUrl('/lancamentos');
+        }, 1000);
+      },
+      err => {
+        this.errorHandlerService.handleError(err);
+        setTimeout(() => {
+          this.router.navigateByUrl('/lancamentos');
+        }, 1000);
+      }
+    );
+  }
+
+  private isValidNumber(obj: any): boolean {
+    if (obj === null || obj === undefined) {
+      return false;
+    }
+    return Number.parseInt(obj, 10) > 0;
   }
 
 }
