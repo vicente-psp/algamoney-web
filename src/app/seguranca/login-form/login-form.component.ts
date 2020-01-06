f4b1833ae3e6d493a5b1491a17fd93689260b760
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-05 21:56:12 -0300

    Ajustado método de login e chamado serviço para realizar autenticação.

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ToastyService } from 'ng2-toasty';
+
+import { AuthService } from '../auth.service';
+import { ErrorHandlerService } from './../../core/error-handler.service';
+
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private errorHandlerService: ErrorHandlerService,
+    private toastyService: ToastyService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
-  }
-
-  public login(usuario, senha): void {
+  public login(usuario: string, senha: string): void {
+    this.authService.login(usuario, senha).subscribe(
+      () => {
+        this.toastyService.success('Usuário logado!');
+        this.router.navigateByUrl('/lancamentos');
+      },
+      err => {
+        this.errorHandlerService.handleError(err);
+      }
+    );
   }
 
 }
