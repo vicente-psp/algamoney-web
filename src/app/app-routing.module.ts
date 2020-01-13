0dd58823a63123510de4e9b7a3e5fc7ad799d7ba
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-12 22:52:18 -0200

    Criado componente para redirecionar usuário quando não houver permissão. de acesso | adicionado rota nao-autorizado.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import localePt from '@angular/common/locales/pt';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NaoAutorizadoComponent } from './core/nao-autorizado.component';
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
 registerLocaleData(localePt);
 
 const routes: Routes = [
   { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
+  { path: 'nao-autorizado', component: NaoAutorizadoComponent },
   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
   { path: '**', redirectTo: 'pagina-nao-encontrada' },
 ];
