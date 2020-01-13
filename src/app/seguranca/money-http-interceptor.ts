d770f01dcd6b983ef7525992a050a3a1f3383cd6
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-12 22:57:40 -0200

    Criado interceptador de requisições para renovar o token.

---
+++ src/app/seguranca/money-http-interceptor.ts
@@ -0,0 +1,40 @@
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable, from } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
+
+export class NotAuthenticatedError {}
+
+
+@Injectable()
+export class MoneyHttpInterceptor implements HttpInterceptor {
+
+    constructor(private auth: AuthService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
+
+      if (!req.url.includes('/oauth/token') && this.auth.isAccessTokenInvalido()) {
+
+        return from(this.auth.getRefreshToken())
+          .pipe(
+              mergeMap(() => {
+                if (this.auth.isAccessTokenInvalido()) {
+                  throw new NotAuthenticatedError();
+                }
+                req = req.clone({
+                    setHeaders: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                });
+                return next.handle(req);
+              }
+            )
+          );
+      }
+
+      return next.handle(req);
+    }
+}
