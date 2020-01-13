a13edaafc02749d891109b789278211923e0bd5b
Vicente Silva <vicente.pspenha@gmail.com>
2020-01-12 22:50:06 -0200

    Adicionado verificações de permissão e de não autenticado | obtido msg para usuário retornado da API.

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { ToastyService } from 'ng2-toasty';
+
+import { NotAuthenticatedError } from './../seguranca/money-http-interceptor';
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,23 +12,36 @@ import { ToastyService } from 'ng2-toasty';
 export class ErrorHandlerService {
 
   constructor(
-    private toastyService: ToastyService
+    private toastyService: ToastyService,
+    private router: Router
   ) { }
 
   public handleError(errorResponse: any): void {
     let msg: string;
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigateByUrl('/login');
     } else {
-      if (!this.isNullOrUndefined(errorResponse.error[0]) &&
-       !this.isNullOrUndefined(errorResponse.error[0].userMessage)) {
-         msg = errorResponse.error[0].userMessage;
-         if (!this.isNullOrUndefined(errorResponse.error[0].developerMessage)) {
-           console.error('Ocorreu um erro', errorResponse.error);
-         }
-      } else {
-        msg = 'Erro ao processar serviço remoto. Tente novamente.';
+      if (errorResponse instanceof Response && errorResponse.status === 403) {
+        msg = 'Você não tem permissão para executar esta ação.';
         console.error('Ocorreu um erro', errorResponse);
+      } else {
+        if (errorResponse.status === 403) {
+          msg = 'Você não tem permissão para executar esta ação!';
+        } else {
+          if (!this.isNullOrUndefined(errorResponse.error[0]) &&
+           !this.isNullOrUndefined(errorResponse.error[0].userMessage)) {
+             msg = errorResponse.error[0].userMessage;
+             if (!this.isNullOrUndefined(errorResponse.error[0].developerMessage)) {
+               console.error('Ocorreu um erro', errorResponse.error);
+             }
+          } else {
+            msg = 'Erro ao processar serviço remoto. Tente novamente.';
+            console.error('Ocorreu um erro', errorResponse);
+          }
+        }
       }
     }
     this.toastyService.error(msg);
