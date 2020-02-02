76d94279516f81edf8cd177b53e252f4eb4688fa
Vicente Silva <vicente.pspenha@gmail.com>
2020-02-02 20:25:27 -0200

    Adicionado módulo ReactiveFormsModule.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ErrorMessagesComponent } from './error-messages/error-messages.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,6 +12,7 @@ import { FormsModule } from '@angular/forms';
   exports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
 
     ErrorMessagesComponent,
   ],
